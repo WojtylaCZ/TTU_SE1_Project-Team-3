845e1a4ed573ce1a40b34d6bed0e4016cbdf672b
Isaac Griswold-Steiner <isaac.griswold.steiner@gmail.com>
2016-02-16 12:39:47 -0600

    Initial Setup of Project
    This is the initial setup to make sure that everyone can build the
    project.

---
+++ app/src/test/java/com/ttu_se1_project_team_3/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.ttu_se1_project_team_3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
