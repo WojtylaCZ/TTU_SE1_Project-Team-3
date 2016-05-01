1182511f291b65d4b5efc62bd68c1e7d5b3b6a0f
Isaac Griswold-Steiner <isaac31415926@gmail.com>
2016-05-01 16:42:47 -0500

    I give up on the new Activity testing for now, this is a stopping point. I'll be back...

--- app/src/androidTest/java/com/ttu_se1_project_team_3/WelcomeTests.java
+++ app/src/androidTest/java/com/ttu_se1_project_team_3/WelcomeTests.java
@@ -2,66 +2,42 @@ package com.ttu_se1_project_team_3;
 
 import org.junit.Rule;
 import org.junit.Test;
-
-import android.app.ActivityManager;
-import android.app.Instrumentation;
-import android.support.test.*;
+import org.junit.runner.RunWith;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
-import android.app.Instrumentation.ActivityMonitor;
-import android.app.Instrumentation;
-import android.test.InstrumentationTestCase;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.ttu_se1_project_team_3.activities.Login;
 import com.ttu_se1_project_team_3.activities.Welcome;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import android.util.Log;
-import android.widget.Button;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 /**
  * Created by Isaac on 4/30/2016.
- * UI Tests for the Welcome Activity
+ * UI Tests for the Welcome Activity.
  */
-public class WelcomeTests extends InstrumentationTestCase {
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class  WelcomeTests {
 
     @Rule
-    public ActivityTestRule<Welcome> mActivityRule = new ActivityTestRule<>(
-            Welcome.class);
+    public final ActivityTestRule<Welcome> rule =
+            new ActivityTestRule<>(Welcome.class);
 
-    private final String LOG_TAG = Welcome.class.getSimpleName();
-    private Instrumentation instr;
-    private Welcome welcome = mActivityRule.getActivity();
+    @Test
+    public void test_goToLoginActivity() throws Exception {
+        Intents.init();
 
-    @Override
-    protected void setUp() throws Exception {
-        instr = getInstrumentation();
-        Log.d(LOG_TAG, "setUp instrumentation: " + instr);
-
-        super.setUp();
-
-        instr = getInstrumentation();
-        Log.d(LOG_TAG, "setUp instrumentation: " + instr);
-    }
-
-    public void test_goToLoginActivity() {
-        ActivityMonitor activityMonitor = getInstrumentation().addMonitor(Login.class.getName(), null, false);
-
-        // Type text and then press the button.
         onView(withId(R.id.login_button)).perform(click());
-        final Button button = (Button) welcome.findViewById(R.id.login_button);
-        welcome.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                button.performClick();
-            }
-        });
 
-        Login login = (Login)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
-        assertNotNull(login);
-        login.finish();
+        intended(hasComponent(Login.class.getName()));
+
+        Intents.release();
     }
 }
